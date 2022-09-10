efda9ffedc90fc37445cd497894f7ef67fc4fa5f
Yash Bhanushali <yash.bhanushali@ayraa.io>
2022-09-10 12:24:02 +0530

    added centering behaviour on scroll end

--- src/HomePage/CardContainer.js
+++ src/HomePage/CardContainer.js
@@ -5,6 +5,10 @@ import { totalCardsToPopulate, createIntersectionObserver } from "../utils";
 
 const CardContainer = () => {
   //calc. default card list.
+
+  const timer = useRef(null);
+  const alreadyScrolledTarget = useRef(-1);
+
   let defaultListOfCards = Array.from({ length: 40 }, () =>
     Math.floor(Math.random() * 40)
   );
@@ -46,15 +50,38 @@ const CardContainer = () => {
       inline: "center",
       block: "center",
     });
+    alreadyScrolledTarget.current = activeCardIndexState;
   };
 
   const observer = createIntersectionObserver(setActiveCardIndexState);
 
   const listToRender = [...firstHalf, ...cards, ...secondHalf];
 
+  const centerTheCardWhenScrollingStopped = () => {
+    if (timer.current !== null) {
+      clearTimeout(timer.current);
+    }
+    timer.current = setTimeout(() => {
+      if (alreadyScrolledTarget.current !== activeCardIndexState) {
+        let target = document.getElementById(activeCardIndexState);
+        target.scrollIntoView({
+          behavior: "smooth",
+          inline: "center",
+          block: "center",
+        });
+        alreadyScrolledTarget.current = activeCardIndexState;
+      }
+    }, 200);
+  };
+
   return (
     <>
-      <div className="container" id="scroller-container" ref={containerRef}>
+      <div
+        className="container"
+        id="scroller-container"
+        ref={containerRef}
+        onScroll={centerTheCardWhenScrollingStopped}
+      >
         {listToRender.map((card, index) => (
           <Card
             key={`card-index-${index}`}
